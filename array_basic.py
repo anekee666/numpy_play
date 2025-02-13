2c9b824d196543da91614c2e42a62e8774dc022d
Houri, Shay <shay_houri@bmc.com>
2025-02-13 11:47:16 +0200

    array basic

---
+++ array_basic.py
@@ -0,0 +1,12 @@
+import numpy as np
+A = np.array([1,2,3])
+for e in A:
+  print(e)
+print(A + np.array([4])) #array([5, 6, 7])
+print(A + np.array([4,5,6])) #array([5, 7, 9])
+print(2 * A) #array([2, 4, 6])
+print(A**2) #array([1, 4, 9])
+print(np.sqrt(A))
+print(np.log(A))
+print(np.exp(A))
+print(np.tanh(A))
